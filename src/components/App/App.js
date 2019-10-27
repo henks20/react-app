efbebc8283bb9a54a97a9b4130cad4d16acd032a
Grzegorz Sienkiewicz <grzegorz.sienkiewicz@eu.jll.com>
2019-10-27 11:45:33 +0100

    Implement animated switch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,28 @@
 import * as React from 'react';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout';
 import Faq from '../Faq/Faq';
 import List from '../List/ListContainer';
+import { AnimatedSwitch } from 'react-router-transition';
+import styles from './App.scss';
 
 const App = () => (
   <BrowserRouter>
     <MainLayout>
-      <Switch>
-        {/* TODO change switch to switchanimated  */}
+      <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className={styles.switchWrapper}
+      >
+
         <Route exact path='/' component={Home} />
         <Route exact path='/info' component={Info} />
         <Route exact path='/faq' component={Faq} />
         <Route exact path="/list/:id" component={List} />
-      </Switch>
+      </AnimatedSwitch>
     </MainLayout>
   </BrowserRouter>
 );
