e17eb23e3b02538cf7ca83c38ad7dfef18c81073
Grzegorz Sienkiewicz <grzegorz.sienkiewicz@eu.jll.com>
2019-11-03 08:53:18 +0100

    Add to header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import * as React from 'React';
+import * as React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import styles from './Header.scss';
 import Container from '../Container/Container';
@@ -22,13 +22,21 @@ class Header extends React.Component {
       <header className={styles.component}>
         <Container>
           <div className={styles.wrapper}>
-            <Link className={styles.logo} to='/'><Icon name={icon} /></Link>
+            <Link className={styles.logo} to='/'>
+              <Icon name={icon} />
+            </Link>
             <Search />
             <nav>
               {/* //klasa zostanie nadana jak link aktywny */}
-              <NavLink exact to='/' activeClassName='active'>Home</NavLink>
-              <NavLink exact to='/info' activeClassName='active'>Info</NavLink>
-              <NavLink exact to='/faq' activeClassName='active'>FAQ</NavLink>
+              <NavLink exact to='/' activeClassName='active'>
+                Home
+              </NavLink>
+              <NavLink exact to='/info' activeClassName='active'>
+                Info
+              </NavLink>
+              <NavLink exact to='/faq' activeClassName='active'>
+                FAQ
+              </NavLink>
             </nav>
           </div>
         </Container>
@@ -36,7 +44,5 @@ class Header extends React.Component {
     );
   }
 }
-
-
 
 export default Header;
